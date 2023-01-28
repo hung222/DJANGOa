4789606120177fc0704aeba53d4b517a49cfb7ed
hoyiu <jamety@gmail.com>
2023-01-28 10:30:58 +0700

    upload thu muc tygh vo github

---
+++ gh1/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+from. models import ghgh
+admin.site.register(ghgh)
---
+++ gh1/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Gh1Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gh1'
