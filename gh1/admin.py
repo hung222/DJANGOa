29e1ad35803e2c17340271e95e9ccaa7e7f709a4
hung222 <90371118+hung222@users.noreply.github.com>
2023-01-28 11:51:53 +0700

    Delete gh1 directory

--- gh1/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-from. models import ghgh
-admin.site.register(ghgh)
--- gh1/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-class ghgh(models.Model):
-       tentg = models.CharField(max_length=800)
-       theloai = models.CharField(max_length=1200)
-       chucnang = models.CharField(max_length=1500)
